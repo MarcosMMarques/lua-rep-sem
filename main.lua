0e2357d1c24bcd1eb7c75b1f7b2a0e332fd3cd87
Caio Veloso Dias <105723352+caadjo@users.noreply.github.com>
2025-06-23 09:53:19 -0300

    Update indentation to follow software engineering standards

--- main.lua
+++ main.lua
@@ -4,14 +4,14 @@ local Endereco = require("endereco")
 local Entrega = require("entrega")
 local Restricao = require("restricao")
 
-print("[LOG] Criando veículo tipo Moto...")
+print("Criando veículo tipo Moto")
 local moto = Moto:new(1, 2.0, 60, 200, 15, { Restricao.ESTRADA_DE_TERRA })
 
-print("[LOG] Criando endereços de origem e destino...")
+print("Criando endereços de origem e destino")
 local origem = Endereco:new("Rua A", "39400-000", "Centro", 123, "Montes Claros", "MG", 0, 0)
 local destino = Endereco:new("Rua B", "39400-001", "Centro", 456, "Montes Claros", "MG", 3, 4)
 
-print("[LOG] Criando entrega...")
+print("Criando entrega")
 local entrega = Entrega:new(moto, origem, destino, {})
 
-print("[RESULTADO] Tempo estimado: " .. entrega:calcularTempo() .. " horas")
+print("Tempo estimado: " .. entrega:calcularTempo() .. " horas")
